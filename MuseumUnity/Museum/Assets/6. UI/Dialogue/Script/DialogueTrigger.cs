ce49bb1e09f2a7041d0daa6cb5adabc2482e2112
Stefano RIva <blackashexe@gmail.com>
2021-02-07 17:01:26 +0100

    palace

--- MuseumUnity/Museum/Assets/6. UI/Dialogue/Script/DialogueTrigger.cs
+++ MuseumUnity/Museum/Assets/6. UI/Dialogue/Script/DialogueTrigger.cs
@@ -53,8 +53,8 @@ public class DialogueTrigger : MonoBehaviour
         for(int i=0; i<d.sentences.Length; i++)
         {
 
-            if (!(d.sound[i].Equals("None")))
-                FindObjectOfType<DialogueSoundManager>().Play(d.sound[i]);
+            /*if (!(d.sound[i].Equals("None")))
+                FindObjectOfType<DialogueSoundManager>().Play(d.sound[i]);*/
             yield return new WaitForSeconds(d.time[i]);
             NextQuestion();
         }
